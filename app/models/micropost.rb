e5ce6ab75ecd621890fe8bfae2115d6f8fd0f54f
uzerifth <uzerifth@gmail.com>
2016-05-15 23:01:13 -0400

    Finished Toy App

---
+++ app/models/micropost.rb
@@ -0,0 +1,6 @@
+class Micropost < ActiveRecord::Base
+
+  belongs_to :user
+  validates :content, length: {maximum: 140}
+  validates :content, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+  has_many :microposts
+  validates :email, presence: true
+  validates :name, presence: true
 end
